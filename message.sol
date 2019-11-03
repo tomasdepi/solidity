fb99bbe5d82baa52ff3037f6b6eb1038a03483c2
Tomas De Pietro <tomas.depietro@prosoftwarefactory.com.ar>
2019-11-03 00:17:01 -0300

    add methods get balance and transfer

--- message.sol
+++ message.sol
@@ -15,6 +15,26 @@ contract MessageStore is Ownable{
     function getMessage() public view returns (string) { // view: no se almacenara ningun valor en el smart contract que por lo tanto requiera minar una transaccion dentro de un bloque
         return message;
     }
+
+    //el balance siempre es retornado en way
+    //el balance es cuanta plata tiene elde contrato
+    function getBalance() public view returns (uint){
+    	return address(this).balance;
+    }
     
+    function getBalanceInEther() public view returns(uint){
+    	return getBalance() / 1e18;
+    }
     
+    function transfer(uint amount) public isOwner{
+    	require(address(this).balance >= amount);
+    	owner.transfer(amount); //todas las var de tipo address tienen el methodo transfer
+    }
+
+    function transferTo(uint amount, address to) public isOwner{
+    	require(address(this).balance >= amount);
+    	require(to != address(0)); //evitar cuentas vacias
+
+    	to.transfer(amount);
+    }
 }
